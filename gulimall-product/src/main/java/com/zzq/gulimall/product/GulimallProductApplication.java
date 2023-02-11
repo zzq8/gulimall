0389223953c82a4ca2dfed78e3261d26739ce889
XD <547061946@qq.com>
2023-02-11 14:22:40 +0800

    有这个注解会导致 lombok 注解不能正常编译,项目修复正常

--- gulimall-product/src/main/java/com/zzq/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/zzq/gulimall/product/GulimallProductApplication.java
@@ -1,11 +1,15 @@
 package com.zzq.gulimall.product;
 
+import com.zzq.common.config.GulimallSessionConfig;
+import com.zzq.common.config.MyThreadConfig;
+import com.zzq.common.config.ThreadPoolConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
 
-//@Import({GulimallSessionConfig.class, ThreadPoolConfigProperties.class, MyThreadConfig.class })
+@Import({GulimallSessionConfig.class, ThreadPoolConfigProperties.class, MyThreadConfig.class })
 @EnableFeignClients(basePackages = "com.zzq.gulimall.product.feign")
 @EnableDiscoveryClient
 @SpringBootApplication
